a5875adc9711e5291afffd4a8776431d1a712864
Nick Cook <nick@considerate.software>
2021-06-18 11:45:58 -0700

    Collapsed @available statements
    Observable now supports ExpressibleByNilLiteral to automatically synthesize initializers for values that can be nil
    
    Observable now has a "\.binding" property that pumps out a SwiftUI Binding type.

--- Sources/Observable/Observable.swift
+++ Sources/Observable/Observable.swift
@@ -1,44 +1,38 @@
-import Foundation
+import SwiftUI
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Observable: Encodable where Value: Encodable { }
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Observable: Decodable where Value: Decodable { }
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Observable: Equatable where Value: Equatable {
 	public static func == (lhs: Observable<Value>, rhs: Observable<Value>) -> Bool {
 		lhs.wrappedValue == rhs.wrappedValue
 	}
 }
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Observable: Hashable where Value: Hashable {
 	public func hash(into hasher: inout Hasher) {
 		wrappedValue.hash(into: &hasher)
 	}
 }
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Observable: Identifiable where Value: Identifiable {
 	public var id: Value.ID {
 		wrappedValue.id
+	}
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+extension Observable where Value: ExpressibleByNilLiteral {
+	
+	/// Creates an `Observable` without an initial value.
+	public convenience init() {
+		self.init(wrappedValue: nil)
 	}
 }
 
@@ -48,10 +42,7 @@ extension Observable: Identifiable where Value: Identifiable {
 /// For SwiftUI, either observe the value directly using `@ObservedObject` or use this package's view `Observing` to observe changes locally within any view's body.
 ///
 /// If the value is `Equatable`, new values won't be published unless they are different (`!=`) from before.
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 @propertyWrapper
 public final class Observable<Value>: ObservableObject {
 	public convenience init(from decoder: Decoder) throws where Value: Decodable {
@@ -72,5 +63,9 @@ public final class Observable<Value>: ObservableObject {
 	
 	public var projectedValue: Observable<Value> {
 		self
+	}
+	
+	public var binding: Binding<Value> {
+		Binding(get: { self.wrappedValue }, set: { self.wrappedValue = $0 })
 	}
 }
